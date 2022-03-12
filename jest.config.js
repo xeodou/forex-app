1a5de5bed1d26fc4aea151d258d0d7fa8ecd2a3d
xeodou <xeodou@gmail.com>
2022-03-12 17:26:53 +0800

    chore(*): add jest and husky config.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
