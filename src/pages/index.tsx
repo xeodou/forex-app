444d4f77462ade1a4d665e3e736ac7e078afcf57
xeodou <xeodou@gmail.com>
2022-03-13 21:46:56 +0800

    fix(homepage): update the homepage to load the forex rates.

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { BidsList } from "components/bids-list";
+import { CurrencySwap } from "components/currency-swap";
+import { IconFresh } from "components/icons";
+import { useCurrencyPair, useForexRates } from "hooks";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  const [currencyPair] = useCurrencyPair();
+
+  const { forexRates, reset } = useForexRates(currencyPair[0], currencyPair[1]);
+
+  return (
+    <div>
+      <Head>
+        <title>Foreign Exchange Application</title>
+        <meta name="description" content="Realtime foreign exchange for broker" />
+      </Head>
+      <main className="container max-w-2xl mx-auto px-4 sm:p-6 bg-white overflow-hidden">
+        <header className="flex justify-between">
+          <CurrencySwap />
+          <button aria-label="Refresh" onClick={reset}>
+            <IconFresh />
+          </button>
+        </header>
+        <BidsList rates={forexRates}></BidsList>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
