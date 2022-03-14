8965b84411e7d0e82cc1e708687afc285e9f904c
xeodou <xeodou@gmail.com>
2022-03-14 22:43:39 +0800

    fix(e2e): fix the test setup.

--- cypress/e2e/home.spec.js
+++ cypress/e2e/home.spec.js
@@ -1,8 +1,7 @@
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+describe("Navigation", () => {
+  it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
-
-    expect(cy.title()).to.equal('Foreign Exchange Application');
-  })
-})
+    cy.visit("/");
+    cy.title().should("eq", "Foreign Exchange Application");
+  });
+});
